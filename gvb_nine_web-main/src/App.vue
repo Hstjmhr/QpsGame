3b1e096e5ae987f8002e181d7b130f083a039de4
Hstjmhr <18236090075@163.com>
2025-04-09 21:13:25 +0800

    博客项目

---
+++ gvb_nine_web-main/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <RouterView/>
+</template>
+
+
+<script setup lang="ts">
+import {RouterLink, RouterView} from 'vue-router'
+import {useStore} from "@/stores";
+
+const store = useStore()
+store.loadTheme()
+store.loadToken()
+store.getBigModelInfo()
+</script>
